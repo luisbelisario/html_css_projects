6358ffc1f977f57fc70e90533b462dae5b80b03f
luisbelisario <luisbls77@hotmail.com>
2021-12-14 10:10:39 -0300

    Adicionado exemplo sidenav

---
+++ sidenav/scripts.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.getElementById('menu').style.width = '250px'
+    document.getElementById('main').style.margin = '0 250px'
+}
+
+function closeMenu() {
+    document.getElementById('menu').style.width = '0'
+    document.getElementById('main').style.margin = '0'
+}
